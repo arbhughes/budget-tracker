2303d854a8309d610a1005b1a9a7a9d4915e3aca
Alex Hughes <alex.rb.hughes@gmail.com>
2025-09-24 21:41:54 +0100

    Add CampaignViewSet

--- backend/budget_tracker/views.py
+++ backend/budget_tracker/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Campaign
+from .serializers import CampaignSerializer
 
-# Create your views here.
+
+class CampaignViewSet(viewsets.ModelViewSet):
+    """
+    Provides API actions for Campaign objects:
+    - list existing campaigns
+    - create new campaigns
+    """
+
+    queryset = Campaign.objects.all()
+    serializer_class = CampaignSerializer
