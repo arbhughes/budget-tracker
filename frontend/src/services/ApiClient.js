d07a04886f89f25926cb612720752d823bbfe545
Alex Hughes <alex.rb.hughes@gmail.com>
2025-09-26 00:06:17 +0100

    Update server URL

--- frontend/src/services/ApiClient.js
+++ frontend/src/services/ApiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: `${process.env.REACT_APP_BASE_URL}/api`,
+  baseURL: `${process.env.REACT_APP_SERVER_BASE_URL}/api`,
   headers: {
     "Content-Type": "application/json",
   },
