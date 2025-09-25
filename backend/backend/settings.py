950da54f5921097105a4fcb9d14ccef84d2d21c5
Alex Hughes <alex.rb.hughes@gmail.com>
2025-09-25 20:53:58 +0100

    TODO

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -19,6 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
+# TODO - deal with this key?
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-_g47we8(#*2a*s=$^(ao__-n)(yfymvhux4ue*$18!ie5y&_ax'
 
